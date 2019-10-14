d43032ab13b81c3e4bb5690cd52e2339001a36c1
itechdom <oalghnmi@gmail.com>
2019-10-14 09:45:26 -0400

    first commit

---
+++ MongoDb/models/settings.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// set up a mongoose model
+let settingsSchema = new Schema({
+  title: String,
+  serverLocation: String,
+  image: String,
+  gallery: Array,
+  type: String,
+  isASeed: { type: Boolean, default: false },
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+	createdAt: { type: Date, default: Date.now },
+  resource: { type: String, default: "settings" }
+});
+module.exports = mongoose.model("Settings", settingsSchema);
