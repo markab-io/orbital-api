eb61e48a9134635e8f47b449f4d5418f570ea093
itechdom <oalghnmi@gmail.com>
2019-10-16 16:05:29 -0400

    multiple entries removed from build process

--- Settings/index.js
+++ Settings/index.js
@@ -138,4 +138,4 @@ const Settings = ({ config, settingsModel, permissionsModel, formsModel }) => {
   return [settingsApi, fileUploadApi, formsApi];
 };
 
-module.exports =  Settings;
+module.exports = Settings; Settings;
