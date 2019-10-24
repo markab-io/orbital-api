29252bc18156dfca1324964054086db6b3ffaa3e
itechdom <oalghnmi@gmail.com>
2019-10-24 14:58:38 -0400

    settings

--- MongoDb/models/settings.js
+++ MongoDb/models/settings.js
@@ -1,16 +1,11 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 // set up a mongoose model
 let settingsSchema = new Schema({
-  title: String,
-  serverLocation: String,
-  image: String,
-  gallery: Array,
-  type: String,
-  isASeed: { type: Boolean, default: false },
-  user: { type: Schema.Types.ObjectId, ref: "User" },
-	createdAt: { type: Date, default: Date.now },
+  key: String,
+  fields: { type: Array, default: [] },
+  createdAt: { type: Date, default: Date.now },
   resource: { type: String, default: "settings" }
 });
-module.exports = mongoose.model("Settings", settingsSchema);
+module.exports = settingsSchema;
